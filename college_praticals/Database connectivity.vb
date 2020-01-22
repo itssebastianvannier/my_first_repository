3a20ba38628ecc2c2c6fbf18e273795f75e3e2c2
adityaverma321 <59736486+adityaverma321@users.noreply.github.com>
2020-01-22 15:23:31 +0530

    Update Database connectivity.vb

--- college_praticals/Database connectivity.vb
+++ college_praticals/Database connectivity.vb
@@ -5,46 +5,48 @@ Public Class Form1
     Dim da As OleDbDataAdapter
     Dim ds As DataSet
     Dim gender As String
+
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        conn = New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\university\student.accdb")
+        conn = New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\IEUser\Documents\Aditya\Database Connectivity\Database Connectivity\Database1.accdb")
         conn.Open()
     End Sub
 
-    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btninsert.Click
-        If RadioButton1.Checked = True Then
-            gender = "male"
+    Private Sub btnInsert_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnInsert.Click
+        If selectMale.Checked = True Then
+            gender = "Male"
         Else
-            gender = "female"
+            gender = "Female"
         End If
-        cmd = New OleDbCommand("insert into tbstudent(roll_no,stud_name,Address,gender,dob,hobbies) values(" + txtrollno.Text + ",'" + txtStuName.Text + "','" + txtAdd.Text + "','" + gender + "','" + Format(DateTimePicker1.Value, "dd-MM-yyyy") + "','" + cmbHobbies.Text + "')", conn)
+        cmd = New OleDbCommand("insert into table1([Roll No],[Student_name],[DOB],[Address],[Gender]) values (" + inputRollNo.Text + ",'" + inputName.Text + "','" + Format(inputDOB.Value, "dd-MM-yyyy") + "','" + inputAddress.Text + "','" + gender + "')", conn)
         cmd.ExecuteNonQuery()
         MsgBox("The record has been Inserted successfully")
     End Sub
 
-    Private Sub Button1_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        If RadioButton1.Checked = True Then
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        If selectMale.Checked = True Then
             gender = "male"
         Else
             gender = "female"
         End If
-        cmd = New OleDbCommand("Update tbstudent set stud_name='" + txtStuName.Text + "',Address='" + txtAdd.Text + "',gender='" + gender + "',dob='" + Format(DateTimePicker1.Value, "dd-MM-yyyy") + "',hobbies='" + cmbHobbies.Text + "' where roll_no=" + txtrollno.Text + "", conn)
+        cmd = New OleDbCommand("Update table1 set [Student_name]='" + inputName.Text + "',[Address]='" + inputAddress.Text + "',[Gender]='" + gender + "',[DOB]='" + Format(inputDOB.Value, "dd-MM-yyyy") + "' where [Roll No]=" + inputRollNo.Text + "", conn)
         cmd.ExecuteNonQuery()
         MsgBox("The record has been Updated successfully")
     End Sub
 
-    Private Sub btnSearch_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSearch.Click
-        da = New OleDbDataAdapter("Select * from tbstudent where roll_no=" + txtrollno.Text + "", conn)
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        da = New OleDbDataAdapter("Select * from table1 where [roll no]=" + inputRollNo.Text + "", conn)
         ds = New DataSet
-        da.Fill(ds, "tbstudent")
-        txtStuName.Text = ds.Tables("tbstudent").Rows(0)("stud_name").ToString
-        txtAdd.Text = ds.Tables("tbstudent").Rows(0)("Address").ToString
-        If ds.Tables("tbstudent").Rows(0)("gender") = "male" Then
-            RadioButton1.Checked = True
+        da.Fill(ds, "table1")
+        inputName.Text = ds.Tables("table1").Rows(0)("Student_name").ToString
+        inputAddress.Text = ds.Tables("table1").Rows(0)("Address").ToString
+        If ds.Tables("table1").Rows(0)("gender") = "Male" Then
+            selectMale.Checked = True
         Else
-            RadioButton2.Checked = True
+            selectFemale.Checked = True
         End If
-        DateTimePicker1.Value = ds.Tables("tbstudent").Rows(0)("dob").ToString
-        cmbHobbies.Text = ds.Tables("tbstudent").Rows(0)("Hobbies").ToString
+        inputDOB.Value = ds.Tables("table1").Rows(0)("DOB").ToString
+        'cmbHobbies.Text = ds.Tables("tbstudent").Rows(0)("Hobbies").ToString
 
     End Sub
 End Class
